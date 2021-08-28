42c9cfbea8d3d5c87c0ea55e70ddbbf441886587
tpsumeta <tpsumeta@gmail.com>
2021-08-28 10:11:39 +0700

    add config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Kitviset",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    plugins: [
-
-    ]
-}
+  ],
+};
